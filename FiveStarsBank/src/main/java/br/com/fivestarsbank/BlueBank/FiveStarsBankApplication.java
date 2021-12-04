2e0247b027171ab674ea38a3baf4020b3b94dd5b
Feruaro <fernandaruaro26@hotmail.com>
2021-12-04 13:31:23 -0300

    código revisado e atualizado

---
+++ FiveStarsBank/src/main/java/br/com/fivestarsbank/BlueBank/FiveStarsBankApplication.java
@@ -0,0 +1,15 @@
+package br.com.fivestarsbank.BlueBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration;
+import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
+
+@SpringBootApplication (exclude = {ContextStackAutoConfiguration.class, ContextRegionProviderAutoConfiguration.class})
+public class FiveStarsBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiveStarsBankApplication.class, args);
+	}
+
+}
