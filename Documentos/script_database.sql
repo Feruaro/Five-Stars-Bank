1afea26e88ebe24269238f23750fd3c3c91929c5
Feruaro <fernandaruaro26@hotmail.com>
2021-12-10 14:54:17 -0300

    update file sql

--- Documentos/script_database.sql
+++ Documentos/script_database.sql
@@ -5,12 +5,6 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema bluebank
--- -----------------------------------------------------
--- -----------------------------------------------------
 -- Schema bluebank
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS bluebank DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
