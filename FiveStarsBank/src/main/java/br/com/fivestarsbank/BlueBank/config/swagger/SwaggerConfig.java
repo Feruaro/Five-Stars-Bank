05db769d144ffc10ac52486721450fa79b876f4a
Feruaro <fernandaruaro26@hotmail.com>
2021-12-13 09:38:23 -0300

    código atualizado e revisado

--- FiveStarsBank/src/main/java/br/com/fivestarsbank/BlueBank/config/swagger/SwaggerConfig.java
+++ FiveStarsBank/src/main/java/br/com/fivestarsbank/BlueBank/config/swagger/SwaggerConfig.java
@@ -7,6 +7,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -33,6 +34,9 @@ public class SwaggerConfig {
 					.title("Five Stars Bank")
 					.description("Sistema de cadastro de clientes e transações entre contas bancárias")
 					.version("1.0.0")
+					.license("Apache License Version 2.0")
+					.licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+					.contact(new Contact("Five Stars Bank", "https://github.com/Feruaro/Five-Stars-Bank", "fivestarspanacademy@hotmail.com"))
 					.build();
 		}
 		
